139e2c73e88205d7b25d3d9c76ce0e384be6678c
aryansoni2811 <onlyaryan2003@gmail.com>
2026-01-07 14:18:53 +0530

    logic updated for the pattern-task2

--- 04_javascript_task/javascript_task2.js
+++ 04_javascript_task/javascript_task2.js
@@ -1,10 +1,20 @@
 function printPattern(size) {
     for (let i = 1; i <= size; i++) {
         for (let j = 1; j <= size; j++) {
-            process.stdout.write(String(Math.min(i, j, size - i + 1, size - j + 1)));
-            process.stdout.write(" "); 
+            process.stdout.write(String(
+                i <= j
+                    ? (i <= size - i + 1
+                        ? (i <= size - j + 1 ? i : size - j + 1)
+                        : (size - i + 1 <= size - j + 1 ? size - i + 1 : size - j + 1))
+                    : (j <= size - i + 1
+                        ? (j <= size - j + 1 ? j : size - j + 1)
+                        : (size - i + 1 <= size - j + 1 ? size - i + 1 : size - j + 1))
+            ));
+            process.stdout.write(" ");
         }
-        console.log(); 
+        console.log();
     }
 }
 printPattern(6);
+
+
