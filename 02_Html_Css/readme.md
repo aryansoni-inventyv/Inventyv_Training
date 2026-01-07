5893608380a3ee779036baee9cdd39fd6dc7ce27
aryansoni2811 <onlyaryan2003@gmail.com>
2026-01-07 14:36:47 +0530

    added readme file for html css task

---
+++ 02_Html_Css/readme.md
@@ -0,0 +1,18 @@
+# HTML CSS practice
+
+- I have created a small website using HTML and CSS
+- In this website , I learned about :
+  - em (for size)
+  - vh (for size)
+  - user-select : none (user selection is prohibited)
+  - @media (for it UI in mobile)
+  - border-radius: 50%; (for round image)
+  - span is used in such a way that it will jump to that specific url 
+  - on clicking login/register/forgetpassword it will assign the url with their repsective hastags by using < a href> anchor tag
+
+- ScreenShots of website     
+    - for Desktop view
+        ![alt text](<Screenshot 2026-01-07 142114-1.png>) 
+    - for Mobile view 
+     
+        ![alt text](<Screenshot 2026-01-07 142133-1.png>) 
--- 04_javascript_task/javascript_task1.js
+++ 04_javascript_task/javascript_task1.js
@@ -1,49 +1,49 @@
 let sum = 0;
 
-const promise1 = new Promise((resolve , reject) => {
-        if(sum > 35){
-            resolve('successful');
-        }else{
-            reject('unsuccessful');
-        }
-    });
+const promise1 = new Promise((resolve, reject) => {
+    if (sum > 35) {
+        resolve('successful');
+    } else {
+        reject('unsuccessful');
+    }
+});
 
-function main1(){
+function main1() {
     let array = [10, 20, 30, 40, 50];
     main2(array);
 }
 
-function main2(arr){
+function main2(arr) {
     // for(let i = 0; i < arr.length; i++){
     //     console.log(arr[i]);
     // }
     // arr[1] = 1;
     // console.log(arr[1]);
 
-    let element = arr[0] ;
-    arr.splice(0 , 1);
+    let element = arr[0];
+    arr.splice(0, 1);
     // console.log("Removed element: " + element);
     // for(let i = 0; i < arr.length; i++){
     //     console.log(arr[i]);
     // }
 
-    main3(element , arr);
+    main3(element, arr);
 
 }
 
-function main3(element , arr){
+function main3(element, arr) {
     // console.log("Element: " + element);
     // for(let i = 0; i < arr.length; i++){
     //     console.log(arr[i]);
     // }
 
-    let array2 = [100 , 200 , 300]; 
+    let array2 = [100, 200, 300];
     array2.unshift(element);
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         array2.push(arr[i]);
     }
-    
-    for(let i = 0; i < array2.length; i++){
+
+    for (let i = 0; i < array2.length; i++) {
         sum += array2[i];
     }
 
@@ -52,13 +52,13 @@ function main3(element , arr){
     // }
 
 
-    
+
 
 
     promise1.then((message) => {
         console.log("The sum is " + sum + ", the operation was " + message);
     }).catch((message) => {
         console.log("The sum is " + sum + ", the operation was " + message);
-    }); 
+    });
 }
 main1();
